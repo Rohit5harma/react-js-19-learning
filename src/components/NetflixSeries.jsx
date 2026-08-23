ac9f7365fd59cad53a06366e374672b53492424c
Rohit Sharma <kaushikrohit6396@gmail.com>
2026-08-23 23:21:19 +0530

    day 4: practice React components and JSON data

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,26 +1,26 @@
-//1. variable 
+// //1. variable 
 
 
-    const NetflixSeries = () =>{
-      const name = "Queen Of tears";
-      const rating = "8.2";
-      const summary = "Baek Hyun-woo and Hong Hae-in navigate a tense relation, both at home and at work. But upon deciding his future, Hyun-woo pays a visit to his family.";
-      let age = 19;
+//     const NetflixSeries = () =>{
+//       const name = "Queen Of tears";
+//       const rating = "8.2";
+//       const summary = "Baek Hyun-woo and Hong Hae-in navigate a tense relation, both at home and at work. But upon deciding his future, Hyun-woo pays a visit to his family.";
+//       let age = 19;
 
 
 
-    // let canWatch = "Not Available";
-    // if(age>=18) canWatch = "Watch Now"
+//     // let canWatch = "Not Available";
+//     // if(age>=18) canWatch = "Watch Now"
 
-    const canWatch = () =>{
-      if(age >= 18)  return "Watch Now";
-      return "Not Available";
-    };
+//     const canWatch = () =>{
+//       if(age >= 18)  return "Watch Now";
+//       return "Not Available";
+//     };
 
-      const returnGenre = () => {
-      const genre = "Romcom"
-     return genre;
-    };
+//       const returnGenre = () => {
+//       const genre = "Romcom"
+//      return genre;
+//     };
 
 
     // if(age < 18 ) {
@@ -54,31 +54,62 @@
 
 
      //One another benefits is also that, you can pass some dynamic value as function parameter 
-       return (  
-          
-            <div>
+      
+     
+     
+     
+     
+      import seriesData from "../api/seriesData.json";
+     
+     const NetflixSeries = () => {
+     return (  
+
+      <ul>
+        {
+          seriesData.map((curElem) => {
+            return (
+
+            <li key = {curElem.id}>
       <div>
-        <img src="queen of tears.jpg" alt="queen of tears.jpg"
-         width="100%" height="50%" />
+        <img 
+        src={curElem.img_url}
+         alt="queen of tears.jpg"
+         width="40%" 
+         height="40%"
+          />
+
       </div>
-      <h2> Name: {name} </h2>
-      <h3>Rating: {rating} </h3>
-      <p>summary: {summary} </p>
-      <p>Genre: {returnGenre()} </p>
+      <h2> Name: {curElem.name} </h2>
+      <h3>Rating: {curElem.rating} </h3>
+      <p>summary: {curElem.description} </p>
+      <p>Genre: {curElem.genre} </p>
+      <p>Cast:{curElem.Cast}</p>
+    <a href={curElem.watch_url}  target="_blank">
+      <button>Watch Now</button>
+      </a>
+
       {/* <button id="btn"> {age >= 18 ? "Watch Now" : "Not Available"}  </button> */}
       {/* <button>{canWatch}</button> */}
-        <button>{canWatch()}</button>
-    </div>
+        
+    </li>
+            );
+
+ })
+        }
+      
     
-      )
+    
+
+    </ul>
+    
+    
+      );
     };
+         
   
 
    
 export default NetflixSeries;
 
 
-export const Footer = () => {
-    return <p> copyright @thapatechnical </p>;
 
-};
