fd323ddeca4d3aff08162e8a514858533281e623
Rohit Sharma <kaushikrohit6396@gmail.com>
2026-09-14 12:51:57 +0530

    learned useState hook and component re-rendering

--- src/components/hokes/state.jsx
+++ src/components/hokes/state.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 export const State = () => {
+  // let value = 0;
+  // const handleButtonClick = () => {
+  //   value++;
+  //   console.log(value);
+  // };
+
   const [value, setValue] = useState(0);
 
   const handleButtonClick = () => {
@@ -11,6 +17,19 @@ export const State = () => {
     <>
       <h1>{value}</h1>
       <button onClick={handleButtonClick}>Increment</button>
+
+      <ChildComponent />
     </>
   );
 };
+
+function ChildComponent() {
+  console.log("Child Component render");
+
+  return (
+    <div className="main-div">
+      { " " }
+      <h2>Child Component</h2>
+    </div>
+  );
+}
